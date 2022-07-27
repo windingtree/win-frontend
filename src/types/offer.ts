a6b9481e2522e5c3b5529bafe7bd2713d4ddeeac
bobetbat <b.bobetayev.97@gmail.com>
2022-07-27 14:37:28 +0200

    chore: added offer types, types moved to separate folder

---
+++ src/types/offer.ts
@@ -0,0 +1,46 @@
+export interface Price {
+  currency: string;
+  public: number;
+  taxes: number;
+}
+
+export interface Fare {
+  usage: string;
+  amount: number;
+  description: string;
+}
+
+export interface PricedItem {
+  fare: Fare[];
+  taxes: any[];
+}
+
+export interface Offer {
+  expiration: Date;
+  price: Price;
+  pricedItems: PricedItem[];
+  disclosures: string[][];
+}
+
+export interface Header {
+  version: string;
+  token: string;
+  supplierId: string;
+  distributorId: string;
+}
+
+export interface RawResponse {
+  bookingToken: string;
+  header: Header;
+}
+
+export interface Data {
+  offerId: string;
+  offer: Offer;
+  rawResponse: RawResponse;
+}
+
+export interface RootObject {
+  data: Data;
+  status: string;
+}
