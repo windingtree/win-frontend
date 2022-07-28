fbb9ad3bf9dba73e3a53fac98c1a769fa506b507
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-07-28 18:06:11 +0200

    refactor: 💡 NetworkSelector component
    Simplified component's code; Added `reset` feature

--- src/components/NetworkSelector.tsx
+++ src/components/NetworkSelector.tsx
@@ -1,71 +1,86 @@
-import { useState, useMemo, useEffect } from 'react';
-import { Select, Spinner } from 'grommet';
-import { allowedNetworks } from '../config';
+import type { NetworkInfo } from '../config';
+import { useState, useEffect, useCallback } from 'react';
+import { Box, Select, Spinner } from 'grommet';
+import { allowedNetworks, getNetworkInfo } from '../config';
 import { useAppState } from '../store';
 import { useNetworkId } from '../hooks/useNetworkId';
 import { useWalletRpcApi } from '../hooks/useWalletRpcApi';
 import { MessageBox } from './MessageBox';
+import Logger from '../utils/logger';
 
-export interface Network {
-  name: string;
-  chainId: number;
-  selected?: boolean;
+const logger = Logger('NetworkSelector');
+
+export interface NetworkSelectorProps {
+  value: NetworkInfo | undefined;
+  onChange: (network?: NetworkInfo) => void;
 }
 
-export const createNetworkOption = (
-  name: string,
-  chainId: number,
-  selectedId: number | undefined
-): Network => ({
-  name: `${name}${chainId === selectedId ? ' ✔' : ''}`,
-  chainId: chainId
-});
-
-export const NetworkSelector = () => {
+export const NetworkSelector = ({ value, onChange }: NetworkSelectorProps) => {
   const { provider } = useAppState();
   const { switchChain } = useWalletRpcApi(provider, allowedNetworks);
-  const [networkId, isLoading, isRightNetwork] = useNetworkId(
-    provider,
-    allowedNetworks
-  );
-  const options = useMemo<Network[]>(
-    () => allowedNetworks.map(n => createNetworkOption(n.name, n.chainId, networkId)),
-    [networkId]
-  );
-  const [network, setNetwork] = useState<Network | undefined>();
-  useEffect(
-    () => {
-      if (networkId) {
-        const currentNetwork = allowedNetworks.find(
-          n => n.chainId === networkId
-        );
-        if (currentNetwork) {
-          setNetwork(createNetworkOption(
-            currentNetwork.name,
-            currentNetwork.chainId,
-            networkId
-          ))
+  const [networkId, isLoading, isRightNetwork] = useNetworkId(provider, allowedNetworks);
+  const [network, setNetwork] = useState<NetworkInfo | undefined>(value);
+  const options = [...allowedNetworks];
+
+  useEffect(() => {
+    const networkCheck = async () => {
+      try {
+        if (value && provider) {
+          const providerNetwork = await provider.getNetwork();
+          if (providerNetwork.chainId !== value.chainId) {
+            logger.debug('Reset: saved value and current chain are different');
+            setNetwork(undefined);
+          }
         }
+      } catch (err) {
+        logger.error(err);
+        setNetwork(undefined);
+      }
+    };
+    networkCheck();
+  }, [provider, value]);
+
+  useEffect(() => {
+    try {
+      if (networkId) {
+        setNetwork(getNetworkInfo(networkId));
       } else {
         setNetwork(undefined);
       }
-    },
-    [networkId]
-  );
-  const setNetworkSelect = async (option: Network) => {
-    try {
-      await switchChain(option.chainId);
-      setNetwork(option);
     } catch (err) {
-      console.log(err);
+      logger.error(err);
+      setNetwork(undefined);
     }
-  };
+  }, [networkId]);
+
+  useEffect(() => onChange(network), [network]);
+
+  const omNetworkChange = useCallback(
+    async (option: NetworkInfo) => {
+      try {
+        if (provider) {
+          const providerNetwork = await provider.getNetwork();
+          if (providerNetwork.chainId !== option.chainId) {
+            logger.debug(`Request provider to change chain #${option.chainId}`);
+            await switchChain(option.chainId);
+            logger.debug(`Network switched to #${option.chainId}`);
+            setNetwork(option);
+          } else {
+            setNetwork(option);
+          }
+        }
+      } catch (err) {
+        logger.error(err);
+      }
+    },
+    [provider]
+  );
 
   return (
-    <>
+    <Box>
       <MessageBox type="warn" show={!isRightNetwork}>
         You are connected to a wrong network. Supported networks:{' '}
-        {allowedNetworks.map((n, i) => n.name + (allowedNetworks.length - 1 !== i ? ', ' : ''))}
+        {options.map((n, i) => n.name + (options.length - 1 !== i ? ', ' : ''))}
       </MessageBox>
       <Select
         placeholder="Select network"
@@ -73,9 +88,9 @@ export const NetworkSelector = () => {
         valueKey="chainId"
         options={options}
         value={network}
-        onChange={({ option }) => setNetworkSelect(option)}
+        onChange={({ option }) => omNetworkChange(option)}
         icon={isLoading ? <Spinner /> : undefined}
       />
-    </>
+    </Box>
   );
 };
