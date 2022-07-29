d9fc680bbd62a540d21e67e792b870b8d3070961
bobetbat <b.bobetayev.97@gmail.com>
2022-07-29 13:01:41 +0200

    fix: facility page fix error

--- src/pages/Facility.tsx
+++ src/pages/Facility.tsx
@@ -3,14 +3,21 @@ import { PageWrapper } from './PageWrapper';
 import { Box, Text, Image } from 'grommet';
 import { useMemo } from 'react';
 import { RoomCard } from '../components/RoomCard';
+import { PricePlansReferences } from '../types/offers';
 
 export const Facility = () => {
-  const { isConnecting, facilities } = useAppState();
+  const { isConnecting, facilities, offers } = useAppState();
   const facility = useMemo(
     () => facilities.find((f) => '/facility/' + f.id === window.location.pathname),
     [facilities]
   );
 
+  const rooms = useMemo(
+    () => facility !== undefined ? offers.find(offer =>
+      Object.keys(offer.pricePlansReferences).includes(facility.id)
+    ) : null,
+    [offers, facility]
+  );
   return (
     <PageWrapper
       breadcrumbs={[
@@ -33,9 +40,9 @@ export const Facility = () => {
               </Text>
             </Box>
           </Box>
-          {facility.roomTypes.map((room, i) => (
+          {/* {rooms && rooms.map((room, i) => (
             <RoomCard key={i} facilityId={facility.id} room={room} />
-          ))}
+          ))} */}
         </Box>
       )}
     </PageWrapper>
