c3953bf4b043632c2b3e3b4cd788e84f5a8acb05
Sil <silkreulen@gmail.com>
2022-08-16 10:45:18 +0200

    feat: include MUI config

---
+++ src/theme/overrides/CssBaseline.ts
@@ -0,0 +1,46 @@
+import { Theme } from '@mui/material/styles';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function CssBaseline(theme: Theme) {
+  return {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '*': {
+          margin: 0,
+          padding: 0,
+          boxSizing: 'border-box'
+        },
+        html: {
+          width: '100%',
+          height: '100%',
+          WebkitOverflowScrolling: 'touch'
+        },
+        body: {
+          width: '100%',
+          height: '100%'
+        },
+        '#__next': {
+          width: '100%',
+          height: '100%'
+        },
+        input: {
+          '&[type=number]': {
+            MozAppearance: 'textfield',
+            '&::-webkit-outer-spin-button': {
+              margin: 0,
+              WebkitAppearance: 'none'
+            },
+            '&::-webkit-inner-spin-button': {
+              margin: 0,
+              WebkitAppearance: 'none'
+            }
+          }
+        },
+        img: {
+          display: 'block',
+          maxWidth: '100%'
+        }
+      }
+    }
+  };
+}
