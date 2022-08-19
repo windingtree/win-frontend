13c015f327b6e4413fbee5917d215647c6e27efa
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-08-19 15:17:19 +0200

    fix: 🐛 Fixed FacilityOffers container

--- src/container/facility/FacilityOffers.tsx
+++ src/container/facility/FacilityOffers.tsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { RoomCard } from 'src/components/RoomCard';
 import { useAccommodationsAndOffers } from 'src/hooks/useAccommodationsAndOffers.tsx';
-import { OfferRecord } from 'src/store/types';
 
 export const FacilityOffers = () => {
   const { getAccommodationById, getOffersById, offers, accommodations } =
@@ -9,11 +8,11 @@ export const FacilityOffers = () => {
   const params = useParams();
   const id: string = params.id as string;
   const accommodation = getAccommodationById(accommodations, id);
-  const matchedOffers: OfferRecord[] = getOffersById(offers, id) as OfferRecord[];
+  const matchedOffers = getOffersById(offers, id);
 
   return (
     <>
-      {matchedOffers.map((offer) => {
+      {matchedOffers.map((offer, index) => {
         //TODO: revise whether we maybe want to restructure the data in such a way that is more intuitive
         // pricePlanReferences has a key that refers to the accommodationId, which can be confusing.
         const accommodationOfOffer = Object.values(offer.pricePlansReferences)[0];
@@ -23,7 +22,7 @@ export const FacilityOffers = () => {
 
         return (
           <RoomCard
-            key={offer.id as string}
+            key={index}
             facilityId={id}
             offer={offer}
             room={matchedRoomWithOffer}
