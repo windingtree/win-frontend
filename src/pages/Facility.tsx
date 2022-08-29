766bebbe44c04938d4cd23160797aaa1dc57ada1
bobetbat <b.bobetayev.97@gmail.com>
2022-08-29 16:16:47 +0200

    feat: mui breadcumbs

--- src/pages/Facility.tsx
+++ src/pages/Facility.tsx
@@ -1,8 +1,30 @@
 import MainLayout from 'src/layouts/main';
 import { FacilityIntroduction } from '../containers/facility/FacilityIntroduction';
 import { FacilityOffers } from 'src/containers/facility/FacilityOffers';
+import { Breadcrumbs } from '../components/Breadcrumbs';
+import { createSearchParams } from 'react-router-dom';
+import { useAccommodationsAndOffers } from '../hooks/useAccommodationsAndOffers.tsx';
+import { useMemo } from 'react';
 
 export const Facility = () => {
+  const { latestQueryParams } = useAccommodationsAndOffers();
+
+  const query = useMemo(() => {
+    console.log('latestQueryParams', latestQueryParams);
+    if (latestQueryParams === undefined) {
+      return '';
+    }
+
+    const params = {
+      roomCount: latestQueryParams.roomCount.toString(),
+      adultCount: latestQueryParams.adultCount.toString(),
+      arrival: latestQueryParams.arrival?.toDateString() ?? '',
+      departure: latestQueryParams.departure?.toDateString() ?? '',
+      location: latestQueryParams.location
+    };
+    return createSearchParams(params);
+  }, [latestQueryParams, createSearchParams]);
+
   return (
     <MainLayout
     //TODO: check with designer whether breadcrumbs are still needed.
@@ -13,6 +35,18 @@ export const Facility = () => {
     //   }
     // ]}
     >
+      <Breadcrumbs
+        links={[
+          {
+            name: 'Home',
+            href: `/`
+          },
+          {
+            name: 'Search',
+            href: `/search?${query}`
+          }
+        ]}
+      />
       <FacilityIntroduction />
       <FacilityOffers />
     </MainLayout>
