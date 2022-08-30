76678836e8d66dd159fd3fcf4a766550f71db503
Sil <silkreulen@gmail.com>
2022-08-30 15:15:29 +0200

    feat: remove comment

--- src/containers/home/LandingHero.tsx
+++ src/containers/home/LandingHero.tsx
@@ -12,7 +12,7 @@ export default function LandingHero() {
         sx={{ marginTop: { xs: 5, md: 10 }, marginBottom: 10, textAlign: 'center' }}
       >
         {/* Should be a h1 element but with h2 styling. Figure out how to do this with MUI */}
-        <Typography variant="h2" mb={2}>
+        <Typography variant="h2" mb={2} component="h1">
           For frens who trav3l
         </Typography>
         <SearchForm />
