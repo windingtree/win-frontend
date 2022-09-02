a0205a3b2589688f81c6e36043e5970a243226dc
bobetbat <b.bobetayev.97@gmail.com>
2022-09-02 15:53:15 +0200

    feat: remove previus response data, show map if no results

--- src/hooks/useAccommodationsAndOffers.tsx/api.ts
+++ src/hooks/useAccommodationsAndOffers.tsx/api.ts
@@ -69,11 +69,13 @@ export async function fetchAccommodationsAndOffers({
   };
 
   const uri = `${process.env.REACT_APP_API_URL}/api/hotels/offers/search`;
-  const { data } = await axios.post<SearchResults>(uri, derbySoftBody).catch((e) => {
-    if (e.response.status === 404) {
-      throw new Error(`No accommodations found for ${location}.`);
-    }
-    throw new Error('Something went wrong. Please try again.');
+  const { data } = await axios.post<SearchResults>(uri, derbySoftBody).catch(() => {
+    return {
+      data: {
+        accommodations: {},
+        offers: {}
+      }
+    };
   });
 
   if (!data) {
