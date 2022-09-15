955c32247730b57a42ee513f048b82aedf682cee
Dave <dave_okanlawon@yahoo.com>
2022-09-15 15:12:49 +0100

    refactor: 💡 create useCurrentEvents hook
    BREAKING CHANGE: 🧨 None

---
+++ src/hooks/useCurrentEvents.ts
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import { NullableDate } from '../utils/date';
+import { getCurrentEvents, getEventsWithinRadius } from '../utils/events';
+import { Coordinates } from './useAccommodationsAndOffers.tsx/api';
+
+export interface CurrentEventsProps {
+  fromDate?: NullableDate;
+  toDate?: NullableDate;
+  center?: Coordinates;
+  radius?: number;
+}
+
+export const useCurrentEvents = ({
+  fromDate,
+  toDate,
+  center,
+  radius = 3
+}: CurrentEventsProps) => {
+  const result = useMemo(() => {
+    const currentEvents =
+      fromDate &&
+      toDate &&
+      // add 1 day swing
+      getCurrentEvents({
+        fromDate: new Date(fromDate.setDate(fromDate.getDate() - 1)),
+        toDate: new Date(toDate.setDate(toDate.getDate() + 1))
+      });
+
+    const initialCenter: [number, number] = center
+      ? [center.lat, center.lon]
+      : [51.505, -0.09];
+
+    const currentEventsWithinRadius =
+      currentEvents && getEventsWithinRadius(currentEvents, initialCenter, radius);
+
+    return currentEventsWithinRadius;
+  }, [fromDate, toDate, center, radius]);
+
+  return result;
+};
