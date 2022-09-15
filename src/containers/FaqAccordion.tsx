1175dc8eccc6c10f1e6642eef58d2ceeaa89ee10
Mathieu Tahon <math@noath.net>
2022-09-15 17:27:13 +0200

    fixed FAQ

--- src/containers/FaqAccordion.tsx
+++ src/containers/FaqAccordion.tsx
@@ -16,12 +16,12 @@ const faq = [
   {
     question: 'What is WIN.so?',
     answer:
-      'Win.so is a web 2.5 accommodation booking platform created by Winding Tree and Simard teams. It operates on smart contract technology and uses crypto payments.'
+      'Win.so is a Web 3.0 accommodation booking platform created by Winding Tree and Simard teams. It operates on smart contract technology and uses crypto-currency payments.'
   },
   {
     question: 'What is Winding Tree?',
     answer:
-      'Winding Tree is one of the first blockchain startups have been building a decentralized marketplace for the travel industry since 2017. Winding tree is a non-profit open source organization gradually transforming into a DAO.'
+      'Winding Tree is one of the first blockchain startups, we have been building a decentralized marketplace for the travel industry since 2017. Winding tree is a non-profit open source organization gradually transforming into a DAO.'
   },
   {
     question: 'What is Simard?',
@@ -30,22 +30,22 @@ const faq = [
   },
   {
     question: 'What is Lif token?',
-    answer: 'Lif token was minted during the ICO fund-raising that was happening in 2018.'
+    answer: 'Lif token was minted during the ICO fund-raising that was happening in 2018. It grants rights to vote on the evolution of the platform.'
   },
   {
-    question: 'Why web 2.5?',
+    question: 'How decentralized is the platform?',
     answer:
-      'Individual hoteliers and accommodation providers as well as corporate partners are not ready for a blockchain merge. Therefore we build a transition to Web 2.5 to make their onboarding process smooth and fast.'
+      'Individual hoteliers and accommodation providers as well as corporate partners are not ready to fully use the blockchain for organizational, technical and legal reasons. Therefore we have built connectivity to their systems via Simard to onboard them.'
   },
   {
     question: 'In how many cities can I book accommodations with WIN.so?',
     answer:
-      'In the first stage, we plan to onboard hotels in the 50 most popular crypto and digital nomad cities.'
+      'We have coverage for any important city globally, but we are currently limited with the currencies we support as we need at least a stablecoin pegged to the local currency of the hotel.'
   },
   {
     question: 'What are you doing about the environmental impact of using blockchain?',
     answer:
-      'Smart contracts WIN is working on are deployed on the Gnosis chain, which is cheap in gas fees and quite sustainable in energy consumption. We also give every traveler a chance to offset the carbon footprint with 10% from each booking.'
+      'Smart contracts WIN is working on are deployed on the Gnosis chain and Polygon, which are cheap in gas fees and quite sustainable in energy consumption. We also give every traveler a chance to offset the carbon footprint with each booking.'
   },
   {
     question: 'How do I book a room with WIN.so?',
@@ -55,12 +55,12 @@ const faq = [
   {
     question: 'Why do you need my personal data?',
     answer:
-      'The platform is still Web 2.5 and hoteliers need clients’ personal data to process the booking and make the reservation on their end. WIN doesn’t keep any of this data for any purposes.'
+      'Hoteliers need clients’ personal data to process the booking and make the reservation on their end. WIN doesn’t use this data for any other purposes.'
   },
   {
     question: 'What is so special about WIN.so?',
     answer:
-      'With WIN you can travel sustainably: either you sustain nature, local brands, or charities, as well as your personal finances. We give you a chance to spend a sum of approximately 10% of the booking in a way you prefer.'
+      'With WIN you can travel sustainably: either you sustain nature or the travel ecosystem. We give you a chance to spend a sum a part of the booking in a way you prefer.'
   },
   {
     question: 'Where can I interact with other travelers who book with WIN.so?',
@@ -69,22 +69,12 @@ const faq = [
   {
     question: 'Can I book with WIN.so with my credit card?',
     answer:
-      'The platform runs on smart contracts, they are fuelled with cryptocurrencies. You can use Moon Pay exchange to buy crypto. But in any case, you need to have your digital wallet. Here is a quick tutorial on how to create your first wallet.'
+      'The platform runs on smart contracts, they are fuelled with cryptocurrencies. You can use exchanges to buy crypto with your card. But in any case, you need to have your digital wallet.'
   },
   {
     question: 'Is there a specific digital wallet needed to book with WIN.so?',
     answer:
-      'At the moment we work with MetaMask and WalletConnect. In the nearest future we plan to integrate others.'
-  },
-  {
-    question: 'How do I download and install MetaMask?',
-    answer:
-      'This is a very step-by-step process. This tutorial will navigate you through the process.'
-  },
-  {
-    question: 'How to buy crypto?',
-    answer:
-      'When you have your wallet created, let’s proceed with the purchase of your first crypto. '
+      'At the moment we work with injected wallets such as MetaMask and WalletConnect. In the future we might integrate others.'
   },
   {
     question: 'What is so special about WIN?',
@@ -94,7 +84,7 @@ const faq = [
   {
     question: 'How to report issues?',
     answer:
-      'If you have found any issues while booking or using our platform, please provide us with the details and print screens to improve@win.so'
+      'If you have found any issues while booking or using our platform, please provide us with the details and print screens on Discord in the Support channel'
   }
 ];
 
