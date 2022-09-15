fbf18cdec9a581d2bc6fcddfdfc5ba413377c8cd
Sil Kreulen <silkreulen@gmail.com>
2022-09-15 16:54:54 +0200

    fix: remove not used variable

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 import { Settings } from 'luxon';
 import { config } from '@windingtree/win-commons';
-import dappcon22 from './images/conferences/dappcon22.jpg';
 import blockchaineurope2022 from './images/conferences/blockchaineurope2022.png';
 import devcon6 from './images/conferences/devcon6.jpeg';
 import ethdownunder from './images/conferences/ethdownunder.png';
