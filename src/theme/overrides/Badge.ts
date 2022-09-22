f70f4f9daa0ab889177b6bc812efb09084167120
Sil Kreulen <silkreulen@gmail.com>
2022-09-22 11:16:10 +0200

    fix: remove uncessary imports

--- src/theme/overrides/Badge.ts
+++ src/theme/overrides/Badge.ts
@@ -1,7 +1,4 @@
-import { Theme } from '@mui/material/styles';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function Badge(theme: Theme) {
+export default function Badge() {
   return {
     MuiBadge: {
       styleOverrides: {
--- src/theme/overrides/Select.tsx
+++ src/theme/overrides/Select.tsx
@@ -1,11 +1,6 @@
-import { Theme } from '@mui/material/styles';
-//
 import { InputSelectIcon } from './CustomIcons';
 
-// ----------------------------------------------------------------------
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function Select(theme: Theme) {
+export default function Select() {
   return {
     MuiSelect: {
       defaultProps: {
