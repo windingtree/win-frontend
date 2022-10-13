d0d08b91f2cea6ab10d319475104321f5efd75e2
Dave <dave_okanlawon@yahoo.com>
2022-10-13 21:23:14 +0100

    fix: 🐛 free solo bug on autocomplete
    BREAKING CHANGE: 🧨 None
    
    ✅ Closes: WIN-731

--- src/components/hook-form/RHFAutocomplete.tsx
+++ src/components/hook-form/RHFAutocomplete.tsx
@@ -27,7 +27,9 @@ export const RHFAutocomplete = <T extends StringOrObject>({
   width = '100%',
   optionValueField = '',
   optionLabelField = 'label',
-  isOptionEqualToValue = (option, value) => option === value,
+  // if freeSolo is true, then arbitrary text is allowed,
+  // and all values are considered valid. In this case, we should always return true
+  isOptionEqualToValue = (option, value) => freeSolo || option === value,
   getOptionLabel,
   ...other
 }: Props<T>) => {
@@ -81,6 +83,7 @@ export const RHFAutocomplete = <T extends StringOrObject>({
             freeSolo={freeSolo}
             isOptionEqualToValue={isOptionEqualToValueFn}
             onChange={changeHandler}
+            onInputChange={changeHandler}
             options={options}
             getOptionLabel={getOptionLabelFn}
             renderInput={({ InputProps, inputProps, ...restParams }) => (
