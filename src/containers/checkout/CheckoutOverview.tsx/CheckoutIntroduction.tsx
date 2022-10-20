91ae844fc6cdaaec28f330b699b1861a39020146
Valera Rozuvan <valera@rozuvan.net>
2022-10-20 12:08:18 +0300

    feat: style fixes and price breakdown

--- src/containers/checkout/CheckoutOverview.tsx/CheckoutIntroduction.tsx
+++ src/containers/checkout/CheckoutOverview.tsx/CheckoutIntroduction.tsx
@@ -2,6 +2,8 @@ import { utils } from 'ethers';
 import { Box, Card, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { formatPrice } from 'src/utils/strings';
+import { currencySymbolMap } from 'src/utils/currencies';
+import { daysBetween } from 'src/utils/date';
 import { useCheckout } from 'src/hooks/useCheckout';
 import { sortByLargestImage } from 'src/utils/accommodation';
 import { useMemo } from 'react';
@@ -12,8 +14,13 @@ export const CheckoutIntroduction = () => {
   const theme = useTheme();
   const { bookingMode, bookingInfo } = useCheckout();
 
-  if (!bookingInfo || !bookingInfo.pricing || !bookingInfo.accommodation) {
-    // don't render anything without pricing info, or accommodation info
+  if (
+    !bookingInfo ||
+    !bookingInfo.pricing ||
+    !bookingInfo.accommodation ||
+    !bookingInfo.date
+  ) {
+    // don't render anything without pricing info, accommodation info, or dates
     return null;
   }
 
@@ -25,17 +32,24 @@ export const CheckoutIntroduction = () => {
   )}`;
 
   const formattedOfferPrice = formatPrice(
-    utils.parseEther(bookingInfo.pricing.offerCurrency.amount.toString()),
-    bookingInfo.pricing.offerCurrency.currency
+    utils.parseEther(bookingInfo.pricing.offerCurrency.amount.toString())
   );
+  const formattedOfferSymbol =
+    currencySymbolMap[bookingInfo.pricing.offerCurrency.currency];
+  const offerCurrency = bookingInfo.pricing.offerCurrency.currency;
 
-  const paymentValue = isGroupMode
-    ? `The refundable deposit is ${formattedOfferPrice}`
-    : `Your payment value is ${formattedOfferPrice}`;
+  const daysOfStay = daysBetween(bookingInfo.date.arrival, bookingInfo.date.departure);
+  let pricePerDay = '0.00';
+  if (daysOfStay > 0) {
+    pricePerDay = (
+      parseFloat(bookingInfo.pricing.offerCurrency.amount) / daysOfStay
+    ).toFixed(2);
+  }
 
   const formattedUsdPrice =
     bookingInfo.pricing.usd &&
     formatPrice(utils.parseEther(bookingInfo.pricing.usd.toString()));
+  const formattedUsdSymbol = currencySymbolMap['USD'];
 
   const showUSDPrice =
     formattedUsdPrice && bookingInfo.pricing.offerCurrency.currency != 'USD';
@@ -54,7 +68,8 @@ export const CheckoutIntroduction = () => {
         borderStyle: 'solid',
         borderColor: theme.palette.text.secondary,
         borderRadius: theme.shape.borderRadius,
-        minWidth: '25em'
+        minWidth: '25em',
+        padding: '1.2em' // TODO: figure out theme default padding value for cards
       }}
     >
       {isGroupMode && (
@@ -74,7 +89,9 @@ export const CheckoutIntroduction = () => {
         </>
       )}
 
-      <Typography mb={{ xs: 3, lg: 2 }}>{accommodationName}</Typography>
+      <Typography mb={{ xs: 3, lg: 2 }} variant="h5">
+        {accommodationName}
+      </Typography>
       {googleMapsLink && (
         <Typography mb={{ xs: 3, lg: 2 }}>
           <a href={googleMapsLink}>see on Google map</a>
@@ -91,9 +108,49 @@ export const CheckoutIntroduction = () => {
           />
         </Card>
       </Box>
-      <Typography mb={{ xs: 3, lg: 2 }}>{paymentValue}</Typography>
+      {isGroupMode && (
+        <Typography mb={{ xs: 3, lg: 2 }} variant="h5">
+          The refundable deposit is {formattedOfferSymbol}
+          {formattedOfferPrice}
+        </Typography>
+      )}
+      {!isGroupMode && (
+        <Typography mb={{ xs: 3, lg: 2 }} variant="h5">
+          Your payment value is {formattedOfferSymbol}
+          {formattedOfferPrice}
+        </Typography>
+      )}
+      <Typography mb={{ xs: 3, lg: 2 }} variant="h5">
+        Price details
+      </Typography>
+      <Typography mb={{ xs: 3, lg: 2 }}>
+        {formattedOfferSymbol} {pricePerDay} x {daysOfStay} nights
+      </Typography>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'row'
+        }}
+      >
+        <Typography mb={{ xs: 3, lg: 2 }}>Total ({offerCurrency})</Typography>
+        <Typography style={{ flexGrow: 1 }}>&nbsp;</Typography>
+        <Typography mb={{ xs: 3, lg: 2 }}>
+          {formattedOfferSymbol} {formattedOfferPrice}
+        </Typography>
+      </Box>
       {showUSDPrice && (
-        <Typography>Equivalent amount in USD ${formattedUsdPrice}</Typography>
+        <Box
+          style={{
+            display: 'flex',
+            flexDirection: 'row'
+          }}
+        >
+          <Typography mb={{ xs: 3, lg: 2 }}>Equivalent amount in USD</Typography>
+          <Typography style={{ flexGrow: 1 }}>&nbsp;</Typography>
+          <Typography mb={{ xs: 3, lg: 2 }}>
+            {formattedUsdSymbol} {formattedUsdPrice}
+          </Typography>
+        </Box>
       )}
     </Box>
   );
