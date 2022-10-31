bf6956dcf07184c2bd14e1464b063e8ca9da2b06
bobetbat <b.bobetayev.97@gmail.com>
2022-10-31 14:02:32 +0100

    refactor: message box new UI

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,7 +1,5 @@
-import { useContext } from 'react';
-import { Box, Button, Spinner, ResponsiveContext } from 'grommet';
-import { StatusInfo, Alert } from 'grommet-icons';
-
+import { Alert, Box, CircularProgress, IconButton } from '@mui/material';
+import Iconify from './Iconify';
 export const allowedMessageBoxTypes = ['info', 'warn', 'error'];
 
 export type MessageBoxTypes = typeof allowedMessageBoxTypes[number];
@@ -21,40 +19,21 @@ export const MessageBox = ({
   children,
   onClose
 }: MessageBoxProps) => {
-  const size = useContext(ResponsiveContext);
-
   if (!show) {
     return null;
   }
 
   return (
-    <Box
-      fill
-      direction="row"
-      background="light-2"
-      align="center"
-      gap={size}
-      pad={size}
-      margin={{ bottom: 'small' }}
-    >
-      <Box>
-        {type === 'info' && <StatusInfo color="status-ok" size={size} />}
-        {type === 'warn' && <Alert color="status-warning" size={size} />}
-        {type === 'error' && <Alert color="status-error" size={size} />}
-      </Box>
-      <Box direction="column" gap={size}>
-        <Box>{children}</Box>
-        {typeof onClose === 'function' && (
-          <Box>
-            <Button primary onClick={onClose} label="close" />
-          </Box>
-        )}
-      </Box>
-      {loading === true && (
-        <Box>
-          <Spinner />
+    <Alert security={type} sx={{ mb: 2, alignItems: 'center' }}>
+      <Box>{children}</Box>
+      {loading === true && <CircularProgress />}
+      {typeof onClose === 'function' && (
+        <Box justifySelf="end">
+          <IconButton onClick={onClose}>
+            <Iconify icon="ci:close-big" />
+          </IconButton>
         </Box>
       )}
-    </Box>
+    </Alert>
   );
 };
